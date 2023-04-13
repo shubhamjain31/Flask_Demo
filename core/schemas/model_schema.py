3d51db88b88d19b439198cb141457ad9bdfb5288
shubhamjain31 <sj27754@gmail.com>
2023-04-13 22:10:09 +0530

    working on response change

--- core/schemas/model_schema.py
+++ core/schemas/model_schema.py
@@ -1,6 +1,7 @@
 from core.database.models import User
 
 from flask_marshmallow import Marshmallow
+from marshmallow import post_dump
 
 Serializer = Marshmallow()
 
@@ -11,6 +12,40 @@ class UserSchema(Serializer.Schema):
         ordered = True
         load_instance = True
         fields = ("id", "username", "email", "name", "password", "is_active", "profile", "date_created")
+
+    @post_dump
+    def change_none_to_string(cls, data, **kwargs):
+        # fields = cls.fields
+        # print(fields)
+        # field_map = {
+        #     "str": "",
+        #     "int": 0,
+        #     "float": 0.0,
+        #     "list": [],
+        #     "dict": {},
+        #     "bool": False,
+        #     "datetime": ""
+        # }
+
+        # field_type = {}
+
+        # for field in fields.items():
+        #     field_type.update({field[0]: field[1]._bind_to_schema.__name__})
+
+        # print(field_type)
+
+        # for key, value in data.items():
+
+        #     try:
+        #         if not value:
+        #             data[key] = field_map[field_type[key]]
+        #     except KeyError:
+        #         pass
+        # return data
+        for field in data:
+            if data[field] is None:
+                data[field] = ""
+        return data
 
 
 user_schema = UserSchema()
