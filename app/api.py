17ffbbaf47b2bfb020b48742d281c379b3fec7bb
shubhamjain31 <sj27754@gmail.com>
2023-04-16 17:39:53 +0530

    working on token authentication

--- app/api.py
+++ app/api.py
@@ -3,33 +3,36 @@ from flask import Blueprint, request
 from core.database.connection import session
 from app.service import user
 
+from utils.decorators import token_required
+
 blueprint = Blueprint("auth", __name__, url_prefix="/api")
 
 @blueprint.route("/users", methods=["GET"])
+@token_required
 def users():
     # all_users = session.query(User).all()
     # return users_schema.dump(all_users)
     response = user.get_multiple(db=session)
-    return {"status":200, "message":'All User!', "data": response}
+    return {"status":200, "message":'All User!', "data": response}, 200
 
 @blueprint.route("/user/<userId>", methods=["GET"])
 def user_(userId: str):
     response = user.get(id=userId, db=session)
-    return {"status":200, "message":'Specific User!', "data": response}
+    return {"status":200, "message":'Specific User!', "data": response}, 200
 
 @blueprint.route("/create-user", methods=["POST"])
 def create_user():
     data = request.get_json()
     response = user.create(db=session, user=data, ip_address=request.remote_addr, user_agent=request.headers.get('User-Agent'))
-    return {"status":201, "message":'User Added!', "data": response}
+    return {"status":201, "message":'User Added!', "data": response}, 200
 
 @blueprint.route("/edit-user/<userId>", methods=["PUT"])
 def edit_user(userId: str):
     data = request.form.to_dict()
     response = user.update(id=userId, db=session, user=data, media=request.files)
-    return {"status":200, "message":'User Updated!', "data": response}
+    return {"status":200, "message":'User Updated!', "data": response}, 200
 
 @blueprint.route("/delete-user/<userId>", methods=["DELETE"])
 def delete_user(userId: str):
     response = user.delete(id=userId, db=session)
-    return {"status":200, "message":'User Deleted!', "data": response}
+    return {"status":200, "message":'User Deleted!', "data": response}, 200
---
+++ utils/decorators.py
@@ -0,0 +1,33 @@
+from functools import wraps
+
+from core.database.models import User
+
+import jwt
+
+from flask import request, jsonify
+
+from decouple import config
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+        # jwt is passed in the request header
+        if 'x-access-token' in request.headers:
+            token = request.headers['x-access-token']
+        # return 401 if token is not passed
+        if not token:
+            return {"status":401, "message":'Token is missing !!!', "data": {}}, 401
+  
+        try:
+            # decoding the payload to fetch the stored details
+            data = jwt.decode(token, config['SECRET_KEY'])
+            current_user = User.query\
+                .filter_by(public_id = data['public_id'])\
+                .first()
+        except:
+            return {"status":401, "message":'Token is invalid !!', "data": {}}, 401
+        # returns the current logged in users context to the routes
+        return  f(current_user, *args, **kwargs)
+  
+    return decorated
